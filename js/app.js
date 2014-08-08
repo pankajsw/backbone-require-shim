6363bf7b890867d25c6a90283628094d2ec975a1
Pankaj Wakchaure <pankaj.wakchaure@synechron.com>
2014-08-08 12:18:29 +0530

    Code Formatting

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 // Filename: app.js
 define([
-  'router', // Request router.js
-], function(Router){
-  var initialize = function(){
-    // Pass in our Router module and call it's initialize function
-    Router.initialize();
-  };
+    'router', // Request router.js
+], function(Router) {
+    var initialize = function() {
+        // Pass in our Router module and call it's initialize function
+        Router.initialize();
+    };
 
-  return { 
-    start: initialize
-  };
+    return {
+        start: initialize
+    };
 });
--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@
 // Require.js allows us to configure shortcut alias
 // Their usage will become more apparent futher along in the tutorial.
 require.config({
-  baseUrl: "js",
-  paths: {
-    jquery: 'libs/jquery/jquery-min',
-    underscore: 'libs/underscore/underscore-min',
-    backbone: 'libs/backbone/backbone-min',
-    templates: '../templates'
-  },
-  shim: {
+    baseUrl: "js",
+    paths: {
+        jquery: 'libs/jquery/jquery-min',
+        underscore: 'libs/underscore/underscore-min',
+        backbone: 'libs/backbone/backbone-min',
+        templates: '../templates'
+    },
+    shim: {
         'backbone': {
             //These script dependencies should be loaded before loading
             //backbone.js
@@ -32,10 +32,10 @@ require.config({
 });
 
 require([
-  // Load our app module and pass it to our definition function
-  'app',
-], function(App){
-  // The "app" dependency is passed in as "App"
-  // Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
-  App.start();
+    // Load our app module and pass it to our definition function
+    'app',
+], function(App) {
+    // The "app" dependency is passed in as "App"
+    // Again, the other dependencies passed in are not "AMD" therefore don't pass a parameter to this function
+    App.start();
 });
